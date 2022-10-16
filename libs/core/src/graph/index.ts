dd38172ddf895273a814709456490b77a104de29
Bowen7 <zwxdyx@foxmail.com>
2022-10-16 23:45:07 +0800

    refactor graph

--- libs/core/src/graph/index.ts
+++ libs/core/src/graph/index.ts
@@ -1,79 +1,74 @@
 import { Node, Edge } from '../types';
-const cloneGraphMap = (map: Map<string, Set<string>>) => {
-  const newMap = new Map();
-  map.forEach((value, key) => {
-    newMap.set(key, new Set(value));
-  });
-  return newMap;
-};
 
 export class Graph {
-  nodes: Node[];
-  edges: Edge[];
-  sourceMap: Map<string, Set<string>> = new Map();
-  targetMap: Map<string, Set<string>> = new Map();
+  nodes: Set<Node> = new Set();
+  edges: Set<Edge> = new Set();
+  sourceMap: Map<string, Map<string, Edge>> = new Map();
+  targetMap: Map<string, Map<string, Edge>> = new Map();
   rankMap: Map<string, number> = new Map();
   constructor(nodes: Node[], edges: Edge[]) {
-    this.nodes = nodes;
-    this.edges = edges;
-    edges.forEach((edge) => {
-      let { source, target } = edge;
-      if (edge.reversed) {
-        [target, source] = [source, target];
-      }
-      if (!this.sourceMap.has(target)) {
-        this.sourceMap.set(target, new Set([source]));
-      }
-      if (!this.targetMap.has(source)) {
-        this.targetMap.set(source, new Set([target]));
-      }
-      this.sourceMap.get(target)!.add(source);
-      this.targetMap.get(source)!.add(target);
-    });
+    nodes.forEach((node) => this.addNode(node));
+    edges.forEach((edge) => this.addEdge(edge));
+  }
+
+  addNode(node: Node) {
+    this.nodes.add(node);
+  }
+
+  addEdge(edge: Edge) {
+    this.edges.add(edge);
+    const { source, target } = edge;
+    if (!this.sourceMap.has(target)) {
+      this.sourceMap.set(target, new Map());
+    }
+    if (!this.targetMap.has(source)) {
+      this.targetMap.set(source, new Map());
+    }
+    this.sourceMap.get(target)!.set(source, edge);
+    this.targetMap.get(source)!.set(target, edge);
   }
 
   clone() {
     const graph = new Graph([], []);
-    graph.nodes = this.nodes.slice();
-    graph.edges = this.edges.slice();
-    graph.sourceMap = cloneGraphMap(this.sourceMap);
-    graph.targetMap = cloneGraphMap(this.targetMap);
+    this.nodes.forEach((node) => graph.addNode(node));
+    this.edges.forEach((edge) => graph.addEdge(edge));
     return graph;
   }
 
-  get rootIds() {
-    return this.nodes
-      .map((node) => node.id)
-      .filter((id) => !this.sourceMap.has(id));
+  get roots() {
+    const roots: Node[] = [];
+    this.nodes.forEach(
+      (node) => !this.sourceMap.has(node.id) && roots.push(node)
+    );
+    return roots;
   }
 
-  get leafIds() {
-    return this.nodes
-      .map((node) => node.id)
-      .filter((id) => !this.targetMap.has(id));
+  get leaves() {
+    const leaves: Node[] = [];
+    this.nodes.forEach(
+      (node) => !this.targetMap.has(node.id) && leaves.push(node)
+    );
+    return leaves;
   }
 
-  // remove edge from sourceMap or targetMap or sourceMap & targetMap
-  removeEdgeFromMap(nodeId: string, type: 'all' | 'target' | 'source' = 'all') {
-    // remove edge from sourceMap
-    if (type !== 'target') {
-      const sourceSet = this.sourceMap.get(nodeId);
-      if (sourceSet) {
-        sourceSet.forEach((source) => {
-          this.targetMap.get(source)?.delete(nodeId);
-        });
-        this.sourceMap.delete(nodeId);
-      }
-    }
-    // remove edge from targetMap
-    if (type !== 'source') {
-      const targetSet = this.targetMap.get(nodeId);
-      if (targetSet) {
-        targetSet.forEach((target) => {
-          this.sourceMap.get(target)?.delete(nodeId);
-        });
-        this.targetMap.delete(nodeId);
-      }
-    }
+  removeEdge(edge: Edge) {
+    const { source, target } = edge;
+    this.edges.delete(edge);
+    this.sourceMap.get(target)?.delete(source);
+    this.targetMap.get(source)?.delete(target);
+  }
+
+  removeEdges(edges: Edge[]) {
+    edges.forEach((edge) => this.removeEdge(edge));
+  }
+
+  sourceEdges(node: Node) {
+    const sources = this.sourceMap.get(node.id);
+    return [...sources!.values()];
+  }
+
+  targetEdges(node: Node) {
+    const targets = this.targetMap.get(node.id);
+    return [...targets!.values()];
   }
 }
