2bd66dc4d590502f5cd0573915b13875f8daad29
Bowen7 <zwxdyx@foxmail.com>
2022-10-17 21:20:49 +0800

    refactor graph

--- libs/core/src/graph/index.ts
+++ libs/core/src/graph/index.ts
@@ -1,8 +1,10 @@
 import { Node, Edge } from '../types';
 
+const getId = (nodeOrId: Node | string) =>
+  typeof nodeOrId === 'string' ? nodeOrId : nodeOrId.id;
 export class Graph {
-  nodes: Set<Node> = new Set();
-  edges: Set<Edge> = new Set();
+  nodeSet: Set<Node> = new Set();
+  edgeSet: Set<Edge> = new Set();
   sourceMap: Map<string, Map<string, Edge>> = new Map();
   targetMap: Map<string, Map<string, Edge>> = new Map();
   rankMap: Map<string, number> = new Map();
@@ -12,11 +14,11 @@ export class Graph {
   }
 
   addNode(node: Node) {
-    this.nodes.add(node);
+    this.nodeSet.add(node);
   }
 
   addEdge(edge: Edge) {
-    this.edges.add(edge);
+    this.edgeSet.add(edge);
     const { source, target } = edge;
     if (!this.sourceMap.has(target)) {
       this.sourceMap.set(target, new Map());
@@ -30,14 +32,14 @@ export class Graph {
 
   clone() {
     const graph = new Graph([], []);
-    this.nodes.forEach((node) => graph.addNode(node));
-    this.edges.forEach((edge) => graph.addEdge(edge));
+    this.nodeSet.forEach((node) => graph.addNode(node));
+    this.edgeSet.forEach((edge) => graph.addEdge(edge));
     return graph;
   }
 
   get roots() {
     const roots: Node[] = [];
-    this.nodes.forEach(
+    this.nodeSet.forEach(
       (node) => !this.sourceMap.has(node.id) && roots.push(node)
     );
     return roots;
@@ -45,7 +47,7 @@ export class Graph {
 
   get leaves() {
     const leaves: Node[] = [];
-    this.nodes.forEach(
+    this.nodeSet.forEach(
       (node) => !this.targetMap.has(node.id) && leaves.push(node)
     );
     return leaves;
@@ -53,22 +55,34 @@ export class Graph {
 
   removeEdge(edge: Edge) {
     const { source, target } = edge;
-    this.edges.delete(edge);
+    this.edgeSet.delete(edge);
     this.sourceMap.get(target)?.delete(source);
     this.targetMap.get(source)?.delete(target);
+  }
+
+  edges(nodeOrId: Node | string) {
+    return [...this.sourceEdges(nodeOrId), ...this.targetEdges(nodeOrId)];
   }
 
   removeEdges(edges: Edge[]) {
     edges.forEach((edge) => this.removeEdge(edge));
   }
 
-  sourceEdges(node: Node) {
-    const sources = this.sourceMap.get(node.id);
+  sourceEdges(nodeOrId: Node | string) {
+    const sources = this.sourceMap.get(getId(nodeOrId));
     return [...sources!.values()];
   }
 
-  targetEdges(node: Node) {
-    const targets = this.targetMap.get(node.id);
+  targetEdges(nodeOrId: Node | string) {
+    const targets = this.targetMap.get(getId(nodeOrId));
     return [...targets!.values()];
+  }
+
+  sourceIds(nodeOrId: Node | string) {
+    return [...(this.sourceMap.get(getId(nodeOrId)) ?? []).keys()];
+  }
+
+  targetIds(nodeOrId: Node | string) {
+    return [...(this.targetMap.get(getId(nodeOrId)) ?? []).keys()];
   }
 }
