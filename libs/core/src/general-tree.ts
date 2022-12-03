78ad40d400a98e06cabe12735e20bd7f7177e046
Bowen7 <zwxdyx@foxmail.com>
2022-12-03 22:54:42 +0800

    commit before World Cup

--- libs/core/src/general-tree.ts
+++ libs/core/src/general-tree.ts
@@ -1,6 +1,6 @@
 type TreeNode = {
-  id?: string;
   children?: TreeNode[];
+  [key: string]: any;
 };
 
 type Options = {
@@ -28,7 +28,7 @@ export class GeneralTree {
   }
 
   getId(node: TreeNode) {
-    const id = node.id || node;
+    const id = node;
     this.nodeMap.set(id, node);
   }
 
@@ -53,7 +53,7 @@ export class GeneralTree {
   }
 
   getNodeById(id: any) {
-    return this.nodeMap.get(id);
+    return this.nodeMap.get(id) || null;
   }
 
   getModifier(node: TreeNode) {
@@ -112,9 +112,38 @@ export class GeneralTree {
 
   // TODO
   apportion(node: TreeNode, level: number) {
-    const leftMost = this.getChildren(node)[0];
+    let leftmost = this.getChildren(node)[0];
+    const neighbor = this.getLeftNeighbor(node);
     const compareDepth = 1;
     const depthToStop = this.maxDepth - level;
+    while (leftmost && neighbor && compareDepth <= depthToStop) {
+      let leftModSum = 0;
+      let rightModSum = 0;
+      let ancestorLeftmost = leftmost;
+      let ancestorNeighbor = neighbor;
+      for (let i = 0; i < compareDepth; i++) {
+        ancestorLeftmost = this.getParent(ancestorLeftmost) as TreeNode;
+        ancestorNeighbor = this.getParent(ancestorNeighbor) as TreeNode;
+        rightModSum += this.getModifier(ancestorLeftmost)!;
+        leftModSum += this.getModifier(ancestorNeighbor)!;
+      }
+
+      if (this.isLeaf(leftmost)) {
+      } else {
+        leftmost = this.getFirstChild(leftmost)!;
+      }
+    }
+  }
+
+  getLeftmost(node: TreeNode, level: number, depth: number) {
+    if (level >= depth) {
+      return Node;
+    } else if (this.isLeaf(node)) {
+      return null;
+    } else {
+      const rightmost = this.getFirstChild(node)!;
+      const leftMost = this.getLeftmost(rightmost, level + 1, depth);
+    }
   }
 
   firstWalk(node: TreeNode, level: number) {
@@ -138,10 +167,10 @@ export class GeneralTree {
         this.setParent(child, node);
         this.firstWalk(child, level + 1);
       });
-      const leftMost = children[0];
-      const rightMost = children[children.length - 1];
+      const leftmost = children[0];
+      const rightmost = children[children.length - 1];
       const midPoint =
-        (this.getPrelim(leftMost)! + this.getPrelim(rightMost)!) / 2;
+        (this.getPrelim(leftmost)! + this.getPrelim(rightmost)!) / 2;
       if (leftSibling) {
         const prelim =
           this.getPrelim(leftSibling)! +
